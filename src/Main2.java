29e135fdc3edea5edca12233e79cdbdaccfb302b
rael <rael06@hotmail.fr>
2019-08-27 15:40:22 +0200

    thread done

--- src/Main2.java
+++ src/Main2.java
@@ -3,9 +3,7 @@ import ReseauSocial.ReseauSocial;
 
 public class Main2 {
     public static void main(String[] args) {
-
         ReseauSocialManager reseauSocialManager = new ReseauSocialManager();
-
         ReseauSocial reseauSocial = new ReseauSocial();
 
     }
